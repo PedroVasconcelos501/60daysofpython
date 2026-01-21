5591ec66133ebc41a9d6a0b4fa8d2097bb372c98
PedroVasconcelos501 <pedroferreiravasconcelos1@gmail.com>
2026-01-21 19:05:09 -0300

    soma

---
+++ dia2/somar_dois_numeros.py
@@ -0,0 +1,7 @@
+numero1 = input("Digite um número: ")
+numero2 = input("Digite outro número: ")
+
+soma_dois_numeros = int(numero1) + int(numero2)
+
+print(f"A soma dos dois números é: {soma_dois_numeros}")
+
---
+++ dia2/somar_dois_numeros2.py
@@ -0,0 +1,15 @@
+def somar (numero1, numero2):
+   somar_dois_numeros = int(numero1) + int(numero2)
+   print(somar_dois_numeros)
+   return somar_dois_numeros
+
+
+
+def somar_dois_numeros ():
+   numero1 = int(input("Digite o primeiro número: "))
+   numero2 = int(input("Digite o segundo número: "))
+   somar_dois_numeros = numero1 + numero2
+   print(somar_dois_numeros)
+   return somar_dois_numeros
+
+somar_dois_numeros()
