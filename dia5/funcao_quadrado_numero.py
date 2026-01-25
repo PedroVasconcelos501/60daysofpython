78b02a74ca95ea2a9579e4c19d3b589153f0b7d5
PedroVasconcelos501 <pedroferreiravasconcelos1@gmail.com>
2026-01-24 22:58:18 -0300

    função que calcula o quadrado de um numero

---
+++ dia5/funcao_quadrado_numero.py
@@ -0,0 +1,9 @@
+#aqui estamos definindo uma função que retorna o quadrado de um número
+def quadrado_numero(numero):
+        return numero ** 2
+    
+def quadrado_numerov2(numero):
+        return numero * numero
+    
+print(quadrado_numero(2))
+print(quadrado_numerov2(2))
