02bae33ee7832f4455c99e2353156f140486fde6
PedroVasconcelos501 <pedroferreiravasconcelos1@gmail.com>
2026-01-28 14:08:22 -0300

    dia 7 dicionario

---
+++ dia7/dicionario_aluno.py
@@ -0,0 +1,13 @@
+#dicionario em python
+
+aluno = {
+    "nome": "João",
+    "idade": 20,
+    "curso": "Engenharia",
+    "matricula" : 123456
+}
+
+print("Nome do aluno:", aluno["nome"])
+print("Idade do aluno:", aluno["idade"])
+print("Curso do aluno:", aluno["curso"])
+print("Matrícula do aluno:", aluno["matricula"])
